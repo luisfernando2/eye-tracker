26804a733430a1d0308fcaabbc0f9ac571e3567b
LeleSilveira <74831693+LeleSilveira@users.noreply.github.com>
2021-08-31 11:36:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const math = window.math;
 import GeometryUtil from "./geometry.util.js";
+import Rosto from "./rosto.js";
 
 /*const botaopos = document.getElementById("botao")*/
 const fundo = document.getElementById("fundo");
 const contexto = fundo.getContext("2d");
+const botao = document.getElementById("botao");
 let mesh;
+var pontox;
+var pontoy;
+var dify = 0;
+var difx = 0;
+var cont = 1;
 
 const NUM_KEYPOINTS = 468;
 const NUM_IRIS_KEYPOINTS = 5;
@@ -18,6 +25,8 @@ function drawLine(ctx, x1, y1, x2, y2) {
   ctx.lineTo(x2, y2);
   ctx.stroke();
 }
+
+var botoes = [];
 
 let output = null;
 let model = null;
@@ -47,9 +56,6 @@ async function setupWebcam() {
   });
 }
 // ---------------------------------------------------------------
-function calculaCoordenadas(x, y) {}
-
-// ---------------------------------------------------------------
 async function trackFace() {
   //DESENHA CÍRCULO DE LIMITAÇÃO
   output.beginPath();
@@ -75,6 +81,7 @@ async function trackFace() {
     video.width,
     video.height
   );
+  //botao
 
   faces.forEach(face => {
     // keypoints  recebe a matriz com os valores x,y,z de cada keypont mapeado
@@ -87,19 +94,31 @@ async function trackFace() {
     let a = [],
       b = [],
       c = [];
-    let x,y;
+    let x, y;
     mesh = face.scaledMesh;
+    const r = new Rosto(mesh);
     let le = mesh[468]; // left eye coordinates
     let re = mesh[473]; // right eye coordinates
-    x = (le[0] + re[0])/2
-    y = (le[1] + re[1])/2
-
+    x = (le[0] + re[0]) / 2;
+    y = (le[1] + re[1]) / 2;
     //TESTE PROJETAR VETORES X,Y,Z
     contexto.clearRect(0, 0, fundo.width, fundo.height);
-    const centroide = math.mean([mesh[470], mesh[472], mesh[469], mesh[471]],0);
-    const { origin, rotationMatrix, pitch, yaw, roll } = computeHeadRotation(face, mesh[4]);
-    drawAxis(contexto, origin, rotationMatrix);  //contexto para seta no meio da tela, output no video
-  
+    const centroide = math.mean(
+      [mesh[470], mesh[472], mesh[469], mesh[471]],
+      0
+    );
+    const {
+      origin,
+      rotationMatrix,
+      pitch,
+      yaw,
+      roll,
+      ptx,
+      pty
+    } = computeHeadRotation(face, mesh[4], difx, dify);
+    pontox = ptx;
+    pontoy = pty;
+    drawAxis(contexto, origin, rotationMatrix); //contexto para seta no meio da tela, output no video
   });
   requestAnimationFrame(trackFace);
 }
@@ -121,7 +140,8 @@ async function trackFace() {
   //comentei translate e scale  resultado: a camera mudou de posicao
   let fundo = document.getElementById("fundo");
 
-  let f = fundo.getContext("2d");
+  const f = fundo.getContext("2d");
+
   fundo.width = video.width;
   fundo.height = video.height;
   f.translate(window.width / 2, window.height / 2); //
@@ -146,22 +166,44 @@ async function trackFace() {
   trackFace();
 })();
 
-function computeHeadRotation(face, origem) {
-  var { origin, rotationMatrix } = GeometryUtil.computeHeadPoseEstimation(face, origem);
+function computeHeadRotation(face, origem, x, y) {
+  var {
+    origin,
+    rotationMatrix,
+    ptx,
+    pty
+  } = GeometryUtil.computeHeadPoseEstimation(face, origem, x, y);
   const { pitch, yaw, roll } = GeometryUtil.rotationMatrixToEulerAngles(
     rotationMatrix
   );
   //const eyesDistance = math.norm(math.subtract(le, re));
-  
+
   let mesh = face.scaledMesh;
 
-  return { origin, rotationMatrix, pitch, yaw, roll };
+  return { origin, rotationMatrix, pitch, yaw, roll, ptx, pty };
 }
 function drawAxis(ctx, origin, rotationMatrix) {
-   let limitX = math.subtract(origin, math.multiply(math.squeeze(math.row(rotationMatrix, 0)), 100.0)).toArray();
-        drawArrow([origin[1], origin[0]], [limitX[1], limitX[0]], "red", 1.0, ctx, 3);
-        let limitY = math.add(origin, math.multiply(math.squeeze(math.row(rotationMatrix, 1)), 100.0)).toArray();
-        drawArrow([origin[1], origin[0]], [limitY[1], limitY[0]], "green", 1.0, ctx, 3);
+  let limitX = math
+    .subtract(
+      origin,
+      math.multiply(math.squeeze(math.row(rotationMatrix, 0)), 100.0)
+    )
+    .toArray();
+  drawArrow([origin[1], origin[0]], [limitX[1], limitX[0]], "red", 1.0, ctx, 3);
+  let limitY = math
+    .add(
+      origin,
+      math.multiply(math.squeeze(math.row(rotationMatrix, 1)), 100.0)
+    )
+    .toArray();
+  drawArrow(
+    [origin[1], origin[0]],
+    [limitY[1], limitY[0]],
+    "green",
+    1.0,
+    ctx,
+    3
+  );
   let limitZ = math
     .subtract(
       origin,
@@ -199,3 +241,20 @@ function drawArrow([ay, ax], [by, bx], color, scale, ctx, lineWidth = 2) {
   ctx.strokeStyle = color;
   ctx.stroke();
 }
+
+const click = function calibra() {  // 5x poderia descartar o maior e o menor valor?
+  cont++;
+  difx = (difx + (pontox[0] - (mesh[473][0] + mesh[468][0]) / 2)) / cont;
+  dify = (dify + (pontoy[1] - mesh[159][1])) / cont;
+  console.log(difx, dify);
+};
+botao.onclick = click;
+
+function draw(x, y, canvas, cor = "#000000") {
+  const ctx = canvas.getContext("2d");
+  ctx.fillStyle = cor;
+  ctx.beginPath();
+  ctx.arc(x, y, 5, 0, Math.PI * 2, true);
+  ctx.fill();
+}
+
